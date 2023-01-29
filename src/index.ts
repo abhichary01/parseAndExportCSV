bd819e37f8e7ca2535d97a4380270a70033f85bd
Abhishek Chary <abhishekchary01@gmail.com>
2023-01-29 12:45:27 +0530

    deploy ready

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,10 @@ import mongoose,{Schema} from 'mongoose';
 import multer, {diskStorage, FileFilterCallback} from 'multer';
 import * as bodyParser from "body-parser"
 import cors from "cors";
-import * as path from 'path';
 import csvtojson from 'csvtojson'
 import Books from './BooksModel'
 import * as json2csv from 'json2csv';
 import * as dotenv from 'dotenv';
-import * as fs from 'fs';
-import * as csv from 'fast-csv'
 import knightMovement from './chessKnight/knightMovement'
 
 
@@ -17,12 +14,12 @@ const app = express();
 dotenv.config();
 
 var corsOptions = {
-  origin: "http://localhost:4000"
+  origin: "http://localhost:4200"
 };
 main().catch(err => console.log(err));
 
 async function main() {
-  const MONGO_DB = process.env.DATABASE_URL || 'Enter your mongo connection string here';
+  const MONGO_DB = process.env.DATABASE_URL || 'mongodb+srv://abhichary01:VZF9QvSiNFpAVBTl@cluster0.zbp60.mongodb.net/?retryWrites=true&w=majority';
   await mongoose.connect(MONGO_DB);
   console.log("Connected to database");
 }
@@ -35,8 +32,16 @@ const storage = multer.diskStorage({
     cb(null, file.originalname)
   },
 })
-const uploads = multer({ storage: storage })
-
+const fileFilter = (req: any, file: { mimetype: string; }, cb: (arg0: null, arg1: boolean) => void) => {
+  if (
+    file.mimetype === 'text/csv'
+  ) {
+    cb(null, true);
+  } else {
+    return cb(null, false);
+  }
+};
+const uploads = multer({ storage: storage, fileFilter: fileFilter })
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -58,6 +63,7 @@ app.get('/', (req, res) => {
 let bookData = [];
 app.post('/upload', uploads.single('csvFile'), (req: any, res: any) => {
   csvtojson()
+  
     .fromFile(req.file.path)
     .then((response: any) => {
       // for (var x = 0; x < response; x++) {
@@ -75,11 +81,12 @@ app.post('/upload', uploads.single('csvFile'), (req: any, res: any) => {
       Books.insertMany(response, (err: any, data: any) => {
         if (err) {
           console.log(err)
+          res.status (400).send ({ message: 'Could not upload the file!', status : 0 })
         } else {
           console.log("data inserted", data);
         }
       })
-      res.json("succesfully uploaded data");
+      res.send({ message: "succesfully uploaded data", status : 1 });
     })
 
 })
@@ -91,37 +98,23 @@ app.get('/getallbooks', async (req, res) => {
 
 app.get('/getbooks/query', async (req, res) => {
   var parameters : any = {};
-  if(req.query.email){
-    parameters['email'] = req.query.email
-  }
-  if(req.query.author){
+  if(req.query.authors !== ''){
     parameters['authors'] = req.query.authors
   }
-  if(req.query.isbn){
+  if(req.query.isbn !== ''){
     let ISBN = (req.query.isbn as string).replace(/(\d{4})(?=\d)/g, "$1-")
-    const num = Number(req.query.isbn);
-    if(isNaN(num)) {
-      res.status(400).json({error: "Not a valid ISBN"});
-    } else {
-      parameters['isbn'] = ISBN;
-    }
+    parameters['isbn'] = ISBN;
   }
-  if (req.query.export) {
-    var result: any = await Books.find(parameters)
-    const objects = result.map((book: { title: any; isbn: any; authors: any; description: any; __v: any; }) => {
-      const {  title, isbn, authors, description} = book;
-      return {  title, isbn, authors, description};
-    });
-    const jsonData = JSON.parse(JSON.stringify(objects));
+  var result: any = await Books.find(parameters)
+  const objects = result.map((book: { title: any; isbn: any; authors: any; description: any; __v: any; }) => {
+    const {  title, isbn, authors, description} = book;
+    return {  title, isbn, authors, description};
+  });
+  const jsonData = JSON.parse(JSON.stringify(objects));
 
-    const csv = json2csv.parse(jsonData);
-    fs.writeFileSync(__dirname+`/output/${req.query.authors||req.query.email||req.query.isbn}.csv`, csv);
-    res.send({ "res": result });
-  }
-  else{
-  const result: any = await Books.find(parameters)
-  res.send({"res": result});
-  }
+  const csv = json2csv.parse(jsonData);
+  // fs.writeFileSync(__dirname+`/output/${req.query.authors||req.query.email||req.query.isbn}.csv`, csv);
+  res.send({ "res": jsonData });
 });
 
 app.post('/insert', async (req: any, res: any) => {
